fb0b4fe5dcb50976776b145ad7b86f95d502865f
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2022-01-24 21:03:04 +0100

    first commit

---
+++ cypress/plugins/index.js
@@ -0,0 +1,9 @@
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
+  on('file:preprocessor', cucumber());
+};
